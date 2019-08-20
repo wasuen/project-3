a39f20c7a08ba0c5fc3472ee49e7616d98f4e798
Austin <wasuen@gmail.com>
2019-08-20 16:55:48 -0700

    add initial register portion

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React , { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch, withRouter } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    username: '',
+    email: '',
+    image: '',
+    loading: true,
+  }
+
+
+
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
